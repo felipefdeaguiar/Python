e5af0aae1badea7da7f16cf7c1c7b6bdd8bfa553
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2020-12-07 09:54:17 -0300

    commit on 'main'

---
+++ udemy - Python (Luiz Otavio Miranda)/teste/teste.py
@@ -0,0 +1,5 @@
+"""
+    curso udemy - Programação em Python 3 do básico ao avançado - Luiz Otavio Miranda
+    07.12.2020 - Felipe Ferreira de Aguiar
+"""
+print('Ola Pessoal ! ')
--- udemy - Python 3 (Luiz Otavio)/exercicios/teste.py
+++
@@ -1,5 +0,0 @@
-"""
-    curso udemy - Programação em Python do básico ao avançado
-    27.11.2020 - Felipe Ferreira de Aguiar
-"""
-print('Ola Pessoal ! ')
