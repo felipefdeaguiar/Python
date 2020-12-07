f0f239a005c51e5628c68a570ad71947350b1f9d
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2020-12-07 11:09:36 -0300

    commit on 'main'

---
+++ Python 3 completo - Luiz Otavio/aulas/aula001.py
@@ -0,0 +1,17 @@
+"""
+    Python 3 completo - programação com Django, PyQT5, Selenium, Regexp, Testes e TDD, POO, Design Patterns GoF, algoritmos - Luiz Otavio Miranda
+    07.12.2020 - Felipe Ferreira de Aguiar
+"""
+
+print('Linha 1  ') # ! Isso é um comentário;
+print('Linha 2  ')
+# print('Linha 3  ') Codigo em comentário para não rodar
+print('Linha 4  ')
+print('Linha 5  ')
+print('Linha 6  ')
+
+"""
+    Comentário em multi linhas
+    1
+    2
+"""
---
+++ Python 3 completo - Luiz Otavio/aulas/aula002.py
@@ -0,0 +1,11 @@
+"""
+    Python 3 completo - programação com Django, PyQT5, Selenium, Regexp, Testes e TDD, POO, Design Patterns GoF, algoritmos - Luiz Otavio Miranda
+    07.12.2020 - Felipe Ferreira de Aguiar
+"""
+
+print('Felipe', 'Ferreira', sep='_', end=' ')
+print('casado com', 'Livia', 'Goulart')
+
+# 824.176.070-18 - Escrever na tela esse CPF usando a função sep e end
+print('824', '176', '070', sep='.', end='-')
+print('18')
