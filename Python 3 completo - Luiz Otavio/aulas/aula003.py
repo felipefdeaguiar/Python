d0b1969d446d8ba61462d4d9be7e2406dee08810
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2020-12-07 11:33:25 -0300

    commit on 'main'

--- Python 3 completo - Luiz Otavio/aulas/aula003.py
+++ Python 3 completo - Luiz Otavio/aulas/aula003.py
@@ -3,3 +3,14 @@
     07.12.2020 - Felipe Ferreira de Aguiar
 """
 
+# !Tipo str que é string
+print('Isso é uma "String" (str).')
+
+# Metodo "\" caracter de escape, não fica bonito.
+print('Isso é uma \'string\' (str).') 
+
+# Metodo "\n" quebra de linha
+print('Ola linha 1 \nOla Linha 2')
+
+# Metodo para o IDE não ler o codigo dentro do Print (r)
+print(r'Ola linha 1 \n Ola linha 2')
