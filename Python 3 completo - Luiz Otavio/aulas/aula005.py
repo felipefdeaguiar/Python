460de0557da8e2ea78ace414452460cfe060dcb5
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2020-12-07 12:38:37 -0300

    commit on 'main'

--- Python 3 completo - Luiz Otavio/aulas/aula005.py
+++ Python 3 completo - Luiz Otavio/aulas/aula005.py
@@ -1,5 +1,30 @@
 """
     Python 3 completo - programação com Django, PyQT5, Selenium, Regexp, Testes e TDD, POO, Design Patterns GoF, algoritmos - Luiz Otavio Miranda
     7.12.2020 - Felipe Ferreira de Aguiar
+    * Operadores aritméticos
+    * + = Adição
+    * - = Subtração
+    * * = Multiplicação
+    * / = Divisão flutuante
+    * // = Divisão inteira
+    * ** = Potenciação
+    * % = resto da Divisão
+    * () = Alteração de precedência
 """
 
+print('Soma 10 + 5 = ', 10 + 5)
+print('Subtração de 10 - 5 = ', 10 - 5)
+print('Multiplicação de 10 * 5 = ', 10 * 5)
+print('Divisão de 10 / 5 = ', 10 / 5)
+print('Divisão inteira de 10 / 4.9 = ', 10 // 4.9)
+print('Potenciação de 10 elevado à quinta potência = ', 10 ** 5)
+print('Resto da divisão de 10 / 5 = ', 10 % 5)
+
+# ! Alteração de precedência matemática ()
+print('Resultado de 5 + 2 * 10 = ', 5+2*10)
+print('Resultado de (5 + 2) * 10 = ', (5+2)*10)
+
+# todo Brincando com os operadores
+print(20*'A')
+print('20' + 'A')
+print(20 + 2)
