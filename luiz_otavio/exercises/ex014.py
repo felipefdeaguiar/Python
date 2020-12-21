81091aab54d7cfc7802f23b6b3dd301d5acb3198
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2020-12-21 10:56:39 -0300

    commit on 'main'

---
+++ luiz_otavio/exercises/ex014.py
@@ -0,0 +1,14 @@
+"""
+    21.12.2020 - Felipe Ferreira de Aguiar
+    Manipulando Strings
+
+    * Strings Indices
+    * Fatiamento de Strings
+    * Funcões built-in len, abs, type, print
+
+    Documentação da aula abaixo:
+    https://docs.python.org/3/library/functions.html 
+    https://docs.python.org/3/library/stdtypes.html
+
+"""
+
