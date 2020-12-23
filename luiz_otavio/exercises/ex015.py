a09f6fded6f3c7584b19205d20c203f54d3a441e
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2020-12-23 10:42:34 -0300

    commit on 'main'

---
+++ luiz_otavio/exercises/ex015.py
@@ -0,0 +1,15 @@
+"""
+    23.12.2020 - Felipe Ferreira de Aguiar
+    Estrutura de repetição While
+"""
+
+# ! Loop infinito "teste"
+while True:
+    nome = input('Qual o seu nome ? ')
+    print(f'Seu nome é {nome} ! Ola')
+
+# ! Estrutura de repeticação 0 à 99 "teste"
+x = 0
+while x < 100:
+    print(x)
+    x = x + 1
