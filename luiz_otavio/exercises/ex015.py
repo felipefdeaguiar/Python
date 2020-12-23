1030f3298ac61b59fe3b716d5a708d1dff99ccfc
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2020-12-23 11:42:00 -0300

    commit on 'main'

--- luiz_otavio/exercises/ex015.py
+++ luiz_otavio/exercises/ex015.py
@@ -15,3 +15,45 @@ while x < 100:
     x = x + 1
 
 print('acabou')
+
+# ! Estrutura dentro de outra estrutura while
+x = 0
+while x < 10:
+    y = 0
+
+    while y < 5:
+        print(f'({x}),({y})')
+        y = y + 1
+    
+    x = x + 1
+
+print('acabou')
+
+# ! Calculadora simples "teste"
+while True:
+    print()
+    num_1 = input('Digite o primeiro numero inteiro ')
+    num_2 = input('Digite o segundo numero inteiro ')
+    operador = input('Digite o operador ')
+
+    if not num_1.isnumeric() or not num_2.isnumeric():
+        print('Voce precisa digitar um valor correto')
+        continue
+
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+
+    # ? Operadores básicos! + , - , * , /
+    if operador == '+':
+        print(num_1 + num_2)
+    elif operador == '-':
+        print(num_1 - num_2)
+    elif operador == '*':
+        print(num_1 * num_2)
+    elif operador == '/':
+        print(num_1 / num_2)
+    else:
+        print('Operador Invalido')
+
+   
+    
