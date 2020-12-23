c99fce797e140871f7ba885dca87429dfa53f8df
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2020-12-23 10:04:11 -0300

    commit on 'main'

--- luiz_otavio/exercises/ex014.py
+++ luiz_otavio/exercises/ex014.py
@@ -15,3 +15,18 @@
 # ! indice positivos [p = 0 , y = 1 .... 2 = 8]
 texto = 'python s2'
 print(texto[8])
+
+# ! indice negativos -[987654321]
+texto = 'python s2'
+print(texto[-9])
+
+# ! teste de print para ocultar ultimo caracter
+url = 'www.google.com/'
+print(url[:-1])
+
+# ! print de um caracter especifico ao outro, porem o ultimo indice não é incluido
+str = 'Felipe Aguiar'
+nova_str = str[2:6]
+print(nova_str)
+
+
