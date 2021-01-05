4e9215baca86c9d5615912fe0f811e521fe36b16
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-01-05 11:49:09 -0300

    commit on 'main'

--- luiz_otavio/exercises/ex018.py
+++ luiz_otavio/exercises/ex018.py
@@ -3,3 +3,11 @@
     For in (Interando Strings) função range (start=0, stop, step=1)
 """
 
+# ! função range (Start, Stop e Step)
+for n in range (0, 11, 1):
+    print(n)
+
+# ! função range (Start, Stop e Step)
+for n in range (20, 10, -1):
+    print(n)
+
