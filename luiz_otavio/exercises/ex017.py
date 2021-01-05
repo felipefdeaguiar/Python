8c4f2c35f338fb7d7e76d8845637943a465dc01c
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-01-05 11:09:52 -0300

    commit on 'main'

--- luiz_otavio/exercises/ex017.py
+++ luiz_otavio/exercises/ex017.py
@@ -10,6 +10,6 @@ frase = 'o rato roeu a roupa do rei de roma'
 tamanho_frase = len(frase)
 contador = 0
 
-while contador < 10:
+while contador < tamanho_frase:
     print(frase[contador], contador)
     contador = contador + 1
