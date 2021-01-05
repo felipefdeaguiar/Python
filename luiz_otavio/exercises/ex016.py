ce146cc97316a909864c8bbd121d4976463c021b
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-01-05 10:43:21 -0300

    commit on 'main'

---
+++ luiz_otavio/exercises/ex016.py
@@ -0,0 +1,12 @@
+"""
+    06.01.2021 - Felipe Ferreira de Aguiar
+    Estrutura de repetição While / Else (Contador / acumulador)
+"""
+contador = 1
+acumulador = 1
+
+while contador <= 10:
+    print(contador, acumulador)
+    acumulador = acumulador + contador
+    contador = contador + 1
+
