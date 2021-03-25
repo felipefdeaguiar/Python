e8368fbac46af1647cb605a3bcbf6318ecc71d27
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-03-25 11:54:11 -0300

    commit on 'main'

--- guanabara/exercises/ex0016.py
+++ guanabara/exercises/ex0016.py
@@ -1,6 +1,10 @@
 """
     curso Python 3 - Exercício Python #016
-    Quebrando um numero
+    Crie um programa que leia um numero real qualquer pelo teclado e mostre na teclado 
+    a sua porção inteira
     25.03.2021 - Felipe Ferreira de Aguiar
 """
+import math
+num = float(input('Digite um numero qualquer '))
+print('O valor digitado foi {} e sua porção inteira é {}'.format(num, math.trunc(num)))
 
