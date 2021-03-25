40956efddc8252fad54c94b40cd39ce037c825da
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-03-25 12:21:43 -0300

    commit on 'main'

--- guanabara/exercises/ex0016.py
+++ guanabara/exercises/ex0016.py
@@ -1,7 +1,6 @@
 """
     curso Python 3 - Exercício Python #016
-    Crie um programa que leia um numero real qualquer pelo teclado e mostre na teclado 
-    a sua porção inteira
+    Crie um programa que leia um numero real qualquer pelo teclado e mostre na teclado a sua porção inteira
     25.03.2021 - Felipe Ferreira de Aguiar
 """
 from math import trunc
