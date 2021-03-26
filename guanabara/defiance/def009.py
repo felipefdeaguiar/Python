07ee4bbe8a62945ac7ea0c4ceb477173bec95505
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-03-26 10:41:40 -0300

    commit  on 'main'

--- guanabara/defiance/def009.py
+++ guanabara/defiance/def009.py
@@ -36,3 +36,5 @@ print(frase.title(),'"Todas as letras iniciais das palavras ficam Maiusculas"\n'
 frase_2 = '   Aprenda Python'
 print(frase_2,'"Frase 2 Original"')
 print(frase_2.strip(),'"Remoção dos espaços vazios no inicio da String"')
+print(frase_2.rstrip(),'"Remoção dos espaços vazios no fim da String"')
+print(frase_2.lstrip(),'"Remoção dos espaços da esquerda da String"')
