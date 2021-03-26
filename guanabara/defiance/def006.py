0fa81c2e244367095cae63f7e6fddf8e553cb37b
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-03-26 09:58:41 -0300

    commit on 'main'

--- guanabara/defiance/def006.py
+++ guanabara/defiance/def006.py
@@ -1,6 +1,6 @@
 """
     curso Python 3 - Aula 006 - tipos primitivos e saída de Dados
-    nessa aula, vamos aprender como funcionam os tipos primitivos no Python e as peculiaridades do int() float() bool() e str(). Além disso, veremos como fazer as primeiras operações com a função print() do Python.
+    Nessa aula, vamos aprender como funcionam os tipos primitivos no Python e as peculiaridades do int() float() bool() e str(). Além disso, veremos como fazer as primeiras operações com a função print() do Python.
     18.10.2020 - Felipe Ferreira de Aguiar
 """
 
--- guanabara/defiance/def009.py
+++ guanabara/defiance/def009.py
@@ -1,6 +1,7 @@
 """
     curso Python 3 - Aula 009 - Utilizando Módulos
-    Nessa aula, 
+    Nessa aula, manipulando cadeias de texto
     26.03.2021 - Felipe Ferreira de Aguiar
 """
 
+#todo 
