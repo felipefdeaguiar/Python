e36ac710482bfe0f36202304276757a98df9e462
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-03-26 09:31:06 -0300

    commit on 'main'

--- guanabara/exercises/ex020.py
+++ guanabara/exercises/ex020.py
@@ -5,10 +5,11 @@
     25.03.2021 - Felipe Ferreira de Aguiar
 """
 
-from random import sample
+from random import shuffle
 a1 = str(input('Digite o nome do aluno 1 '))
 a2 = str(input('Digite o nome do aluno 2 '))
 a3 = str(input('Digite o nome do aluno 3 '))
 a4 = str(input('Digite o nome do aluno 4 '))
-lista = sample([a1, a2, a3, a4], k=4)
+lista = [a1, a2, a3, a4]
+shuffle(lista)
 print(f'A ordem da apresentação é {lista}')
