3e618f7a19c877b8494fb292fd29576c008c5396
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-03-26 10:38:46 -0300

    commit on 'main'

--- guanabara/defiance/def009.py
+++ guanabara/defiance/def009.py
@@ -27,7 +27,12 @@ print('Curso' in frase)
 
 #! Transformação de Strings
 frase = 'Curso em Video Python'
-print(frase.replace('Python', 'Android'))
-print(frase.upper())
-print(frase.lower())
-
+print(frase,'"Frase 1 Original"')
+print(frase.replace('Python', 'Android'),'"Substituição de uma palavra"')
+print(frase.upper(),'"Transformação da string para Maiusculas"')
+print(frase.lower(),'"Transformação da String para Minusculas"')
+print(frase.capitalize(),'"Somente a primeira letra fica Maiusculas"')
+print(frase.title(),'"Todas as letras iniciais das palavras ficam Maiusculas"\n')
+frase_2 = '   Aprenda Python'
+print(frase_2,'"Frase 2 Original"')
+print(frase_2.strip(),'"Remoção dos espaços vazios no inicio da String"')
