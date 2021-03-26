406d26e29edeeaac2fe338b64ab7d44e39805e78
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-03-26 11:03:09 -0300

    commit on 'main'

--- guanabara/defiance/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-frase_2 = '   Aprenda Python'
-print(frase_2,'"Frase 2 Original"')
-print(frase_2.strip(),'"Remoção dos espaços vazios no inicio da String"')
-print(frase_2.rstrip(),'"Remoção dos espaços vazios no fim da String"')
-print(frase_2.lstrip(),'"Remoção dos espaços da esquerda da String"')
