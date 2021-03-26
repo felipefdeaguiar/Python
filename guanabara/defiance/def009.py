4d82cdb54dd9a16055bdced046fc5000e5f6d093
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-03-26 10:54:34 -0300

    commit on 'main'

--- guanabara/defiance/def009.py
+++ guanabara/defiance/def009.py
@@ -38,3 +38,4 @@ print(frase_2,'"Frase 2 Original"')
 print(frase_2.strip(),'"Remoção dos espaços vazios no inicio da String"')
 print(frase_2.rstrip(),'"Remoção dos espaços vazios no fim da String"')
 print(frase_2.lstrip(),'"Remoção dos espaços da esquerda da String"')
+
