d73032649df79dfa07850ec14f337fa9ec5e5953
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-03-26 10:22:16 -0300

    commit on 'main'

--- guanabara/defiance/def009.py
+++ guanabara/defiance/def009.py
@@ -22,3 +22,4 @@ frase = 'Curso em Video Python'
 print(len(frase))
 print(frase.count('o',0,13))
 print(frase.find('deo'))
+print(frase.find('Android'))
--- guanabara/defiance/tempCodeRunnerFile.py
+++ guanabara/defiance/tempCodeRunnerFile.py
@@ -2,3 +2,4 @@ frase = 'Curso em Video Python'
 print(len(frase))
 print(frase.count('o',0,13))
 print(frase.find('deo'))
+print(frase.find('Android'))
