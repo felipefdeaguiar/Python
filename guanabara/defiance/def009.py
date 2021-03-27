3eddc86c2bfa4eebde40df8d8e45b9bf8c355039
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-03-27 11:02:07 -0300

    commit on 'main'

--- guanabara/defiance/def009.py
+++ guanabara/defiance/def009.py
@@ -56,4 +56,16 @@ Welcome Welcome Welcome Welcome Welcome Welcome Welcome Welcome Welcome
 Welcome Welcome Welcome Welcome Welcome Welcome Welcome Welcome Welcome
 Welcome Welcome Welcome Welcome Welcome""")
 
-#todo 
+#todo desafio 022 - crie um programa que leia o nome completo de uma pessoa e mostre:
+#1) O nome com todas as letras maiusculas;
+#2) O nome com todos as letras Minusculas;
+#3) Quantas letras tem sem espaço contar os espaços;
+#4) Quantas letras tem o primeiro nome.
+nome = input('Qual é o seu nome completo ')
+print(f'\nO nome digitado foi {nome}')
+print('Transformando em Maiusculo:',nome.upper())
+print('Transformando em Minusculo:',nome.lower())
+nome_1 = nome.replace(" ","")
+print(f'A quantidade do nome completo são {len(nome_1)} caracteres')
+nome_2 = nome.split()
+print(f'A quantidade do primeiro nome são {len(nome_2[0])} caracteres\n')
