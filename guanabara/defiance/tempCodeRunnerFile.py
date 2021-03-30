7490efdde051808e0cad8be90c9e697b2d82d4df
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-03-30 11:00:05 -0300

    commit on  'main'

--- guanabara/defiance/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-name = input('Digite o nome da sua cidade natal ')
-name_1 = name.split()
-if 'Santo' in name_1[0]:
-    print(f'\nA cidade digita foi "{name}" e ela começa por "Santo"\n')
-else:
-    print(f'\nA cidade digita foi "{name}" e ela não começa por "Santo\n')
