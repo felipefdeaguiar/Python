b8c00a522ea83c2711df33667f3f5e6274969270
felipefdeaguiar <felipeferreiradeaguiar@gmail.com>
2021-04-13 12:53:15 -0300

    commit on 'main'

--- guanabara/exercises/ex023.py
+++ guanabara/exercises/ex023.py
@@ -4,8 +4,14 @@
     13.04.2021 - Felipe Ferreira de Aguiar
 """
 
-num = input('Digite um valor de 1000 à 9999 ')
+num = input('Digite um valor de 0 à 9999 ')
 if num.isdigit():
-    print(f'\nO valor digitado foi {num}\nSua milhar é {num[0]}\nSua centena é {num[1]}\nSua dezena é {num[2]}\nSua unidade é {num[3]}\n')
+    n = int(num)
+    u = n // 1 % 10
+    d = n // 10 % 10
+    c = n // 100 % 10
+    m = n // 1000 % 10
+    print(f'\nO valor digitado foi {num}\nSua milhar é {m}\nSua centena é {c}\nSua dezena é {d}\nSua unidade é {u}\n')
 else:
-    print('Digite um numeral correto')
+    print('Digite um numero correto')
+
